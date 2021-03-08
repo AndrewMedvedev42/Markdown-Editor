f627e9a59febe1f371749932a3b2c1c1d39b0d8f
AndrewMedvedev42 <sanjino56@gmail.com>
2021-03-08 12:15:41 +0200

    initial commit

---
+++ build/precache-manifest.ee072839421a97da3fd8ef8eb23a118d.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ea00f6d8aa2d1401245e00629d2f36fe",
+    "url": "/Markdown-Editor/index.html"
+  },
+  {
+    "revision": "39f7a90ab8beb734867f",
+    "url": "/Markdown-Editor/static/css/main.d99fca67.chunk.css"
+  },
+  {
+    "revision": "6bbb2575db765f6d65f2",
+    "url": "/Markdown-Editor/static/js/2.9d0b99b7.chunk.js"
+  },
+  {
+    "revision": "aef280276b8d6f56e9d8dc46988119d9",
+    "url": "/Markdown-Editor/static/js/2.9d0b99b7.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "39f7a90ab8beb734867f",
+    "url": "/Markdown-Editor/static/js/main.785c41a4.chunk.js"
+  },
+  {
+    "revision": "1a5e0fb94f813a9a05bf",
+    "url": "/Markdown-Editor/static/js/runtime-main.914b7b02.js"
+  }
+]);
