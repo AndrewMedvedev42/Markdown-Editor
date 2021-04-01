8d2a4f033fd1601b1a5235320df6d89967795f7d
AndrewMedvedev42 <sanjino56@gmail.com>
2021-04-01 18:50:58 +0300

    comments added and backgound color changed to #dd99ff

--- src/App.js
+++ src/App.js
@@ -1,19 +1,19 @@
 import React, { useState } from 'react'
 import ReactMarkdown from 'react-markdown'
+//ReactMarkdown is an imported component that renders the typed data
+
 
 function App() {
+  //Takes the data that was typed
   const [markdown, setMarkdown] = useState('# Hello World')
 
   return (
     <main>
       <section className='markdown'>
-        <textarea
-          className='input'
-          value={markdown}
-          onChange={(e) => setMarkdown(e.target.value)}
-        ></textarea>
+        <textarea className='input' value={markdown} onChange={(e) => setMarkdown(e.target.value)}></textarea>
         <article className='result'>
-          <ReactMarkdown>{markdown}</ReactMarkdown>
+          {/* ReactMarkdown is an imported component that renders the typed data */}
+          <ReactMarkdown>{markdown}</ReactMarkdown> 
         </article>
       </section>
     </main>
